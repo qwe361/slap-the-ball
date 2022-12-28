4bb5b2dc9fcc92bb99a77acfd0e1827bdd2a9116
qwe361 <109446039+qwe361@users.noreply.github.com>
2022-12-28 13:27:54 +0800

    Add files via upload

---
+++ C.cs
@@ -0,0 +1,8 @@
+// PE file does not contain any managed metadata.
+
+ICSharpCode.Decompiler.Metadata.PEFileNotSupportedException: PE file does not contain any managed metadata.
+   在 ICSharpCode.ILSpy.LoadedAssembly.<LoadAsync>d__52.MoveNext()
+--- 引发异常的上一位置中堆栈跟踪的末尾 ---
+   在 System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   在 System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+   在 ICSharpCode.ILSpy.TreeNodes.AssemblyTreeNode.Decompile(Language language, ITextOutput output, DecompilationOptions options)
